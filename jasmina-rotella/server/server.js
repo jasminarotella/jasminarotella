01c9e7912c5b5b48a03c36188edf4df8a2941eef
jasminarotella <164342084+jasminarotella@users.noreply.github.com>
2025-03-02 22:33:04 +0100

    deploy render1

--- jasmina-rotella/server/server.js
+++ jasmina-rotella/server/server.js
@@ -8,7 +8,7 @@ import calendarioRoutes from './routes/calendario.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
